ec52f8a060c051a6e704d674ff9527a9fa080366
Julie <julie.nguyen@databricks.com>
2022-10-27 17:23:01 +0800

    Add notebooks and autoML best result

---
+++ 03_best_automl_model_notebook_lightGBM.py
@@ -0,0 +1,425 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # LightGBM training
+# MAGIC This is an auto-generated notebook. To reproduce these results, attach this notebook to the **Julie Nguyen Personal Cluster** cluster and rerun it.
+# MAGIC - Compare trials in the [MLflow experiment](#mlflow/experiments/1879520688768185)
+# MAGIC - Navigate to the parent notebook [here](#notebook/1879520688768186) (If you launched the AutoML experiment using the Experiments UI, this link isn't very useful.)
+# MAGIC - Clone this notebook into your project folder by selecting **File > Clone** in the notebook toolbar.
+# MAGIC 
+# MAGIC Runtime Version: _11.3.x-cpu-ml-scala2.12_
+
+# COMMAND ----------
+
+import mlflow
+import databricks.automl_runtime
+
+target_col = "ChurnBool"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Load Data
+
+# COMMAND ----------
+
+from mlflow.tracking import MlflowClient
+import os
+import uuid
+import shutil
+import pandas as pd
+
+# Create temp directory to download input data from MLflow
+input_temp_dir = os.path.join(os.environ["SPARK_LOCAL_DIRS"], "tmp", str(uuid.uuid4())[:8])
+os.makedirs(input_temp_dir)
+
+
+# Download the artifact and read it into a pandas DataFrame
+input_client = MlflowClient()
+input_data_path = input_client.download_artifacts("1a4ae0ec424b4b6abca1053529c45162", "data", input_temp_dir)
+
+df_loaded = pd.read_parquet(os.path.join(input_data_path, "../../../Downloads/training_data"))
+# Delete the temp data
+shutil.rmtree(input_temp_dir)
+
+# Preview data
+df_loaded.head(5)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Select supported columns
+# MAGIC Select only the columns that are supported. This allows us to train a model that can predict on a dataset that has extra columns that are not used in training.
+# MAGIC `[]` are dropped in the pipelines. See the Alerts tab of the AutoML Experiment page for details on why these columns are dropped.
+
+# COMMAND ----------
+
+from databricks.automl_runtime.sklearn.column_selector import ColumnSelector
+supported_cols = ["Tenure", "TotalSpend", "MonthlySpend"]
+col_selector = ColumnSelector(supported_cols)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Preprocessors
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Numerical columns
+# MAGIC 
+# MAGIC Missing values for numerical columns are imputed with mean by default.
+
+# COMMAND ----------
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer, StandardScaler
+
+num_imputers = []
+num_imputers.append(("impute_mean", SimpleImputer(), ["MonthlySpend", "Tenure", "TotalSpend"]))
+
+numerical_pipeline = Pipeline(steps=[
+    ("converter", FunctionTransformer(lambda df: df.apply(pd.to_numeric, errors="coerce"))),
+    ("imputers", ColumnTransformer(num_imputers)),
+    ("standardizer", StandardScaler()),
+])
+
+numerical_transformers = [("numerical", numerical_pipeline, ["Tenure", "TotalSpend", "MonthlySpend"])]
+
+# COMMAND ----------
+
+from sklearn.compose import ColumnTransformer
+
+transformers = numerical_transformers
+
+preprocessor = ColumnTransformer(transformers, remainder="passthrough", sparse_threshold=0)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Train - Validation - Test Split
+# MAGIC The input data is split by AutoML into 3 sets:
+# MAGIC - Train (60% of the dataset used to train the model)
+# MAGIC - Validation (20% of the dataset used to tune the hyperparameters of the model)
+# MAGIC - Test (20% of the dataset used to report the true performance of the model on an unseen dataset)
+# MAGIC 
+# MAGIC `_automl_split_col_fc4d` contains the information of which set a given row belongs to.
+# MAGIC We use this column to split the dataset into the above 3 sets. 
+# MAGIC The column should not be used for training so it is dropped after split is done.
+
+# COMMAND ----------
+
+# AutoML completed train - validation - test split internally and used _automl_split_col_fc4d to specify the set
+split_train_df = df_loaded.loc[df_loaded._automl_split_col_fc4d == "train"]
+split_val_df = df_loaded.loc[df_loaded._automl_split_col_fc4d == "val"]
+split_test_df = df_loaded.loc[df_loaded._automl_split_col_fc4d == "test"]
+
+# Separate target column from features and drop _automl_split_col_fc4d
+X_train = split_train_df.drop([target_col, "_automl_split_col_fc4d"], axis=1)
+y_train = split_train_df[target_col]
+
+X_val = split_val_df.drop([target_col, "_automl_split_col_fc4d"], axis=1)
+y_val = split_val_df[target_col]
+
+X_test = split_test_df.drop([target_col, "_automl_split_col_fc4d"], axis=1)
+y_test = split_test_df[target_col]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Train classification model
+# MAGIC - Log relevant metrics to MLflow to track runs
+# MAGIC - All the runs are logged under [this MLflow experiment](#mlflow/experiments/1879520688768185)
+# MAGIC - Change the model parameters and re-run the training cell to log a different trial to the MLflow experiment
+# MAGIC - To view the full list of tunable hyperparameters, check the output of the cell below
+
+# COMMAND ----------
+
+import lightgbm
+from lightgbm import LGBMClassifier
+
+help(LGBMClassifier)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Define the objective function
+# MAGIC The objective function used to find optimal hyperparameters. By default, this notebook only runs
+# MAGIC this function once (`max_evals=1` in the `hyperopt.fmin` invocation) with fixed hyperparameters, but
+# MAGIC hyperparameters can be tuned by modifying `space`, defined below. `hyperopt.fmin` will then use this
+# MAGIC function's return value to search the space to minimize the loss.
+
+# COMMAND ----------
+
+import mlflow
+import sklearn
+from sklearn import set_config
+from sklearn.pipeline import Pipeline
+
+from hyperopt import hp, tpe, fmin, STATUS_OK, Trials
+
+# Create a separate pipeline to transform the validation dataset. This is used for early stopping.
+mlflow.sklearn.autolog(disable=True)
+pipeline_val = Pipeline([
+    ("column_selector", col_selector),
+    ("preprocessor", preprocessor),
+])
+pipeline_val.fit(X_train, y_train)
+X_val_processed = pipeline_val.transform(X_val)
+
+def objective(params):
+  with mlflow.start_run(experiment_id="1879520688768185", run_name="lightgbm") as mlflow_run:
+    lgbmc_classifier = LGBMClassifier(**params)
+
+    model = Pipeline([
+        ("column_selector", col_selector),
+        ("preprocessor", preprocessor),
+        ("classifier", lgbmc_classifier),
+    ])
+
+    # Enable automatic logging of input samples, metrics, parameters, and models
+    mlflow.sklearn.autolog(
+        log_input_examples=True,
+        silent=True)
+
+    model.fit(X_train, y_train, classifier__callbacks=[lightgbm.early_stopping(5), lightgbm.log_evaluation(0)], classifier__eval_set=[(X_val_processed,y_val)])
+
+    
+    # Log metrics for the training set
+    lgbmc_training_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_train, y_train, prefix="training_", pos_label=1)
+
+    # Log metrics for the validation set
+    lgbmc_val_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_val, y_val, prefix="val_", pos_label=1)
+
+    # Log metrics for the test set
+    lgbmc_test_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_test, y_test, prefix="test_", pos_label=1)
+
+    loss = lgbmc_val_metrics["val_f1_score"]
+
+    # Truncate metric key names so they can be displayed together
+    lgbmc_val_metrics = {k.replace("val_", ""): v for k, v in lgbmc_val_metrics.items()}
+    lgbmc_test_metrics = {k.replace("test_", ""): v for k, v in lgbmc_test_metrics.items()}
+
+    return {
+      "loss": loss,
+      "status": STATUS_OK,
+      "val_metrics": lgbmc_val_metrics,
+      "test_metrics": lgbmc_test_metrics,
+      "model": model,
+      "run": mlflow_run,
+    }
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Configure the hyperparameter search space
+# MAGIC Configure the search space of parameters. Parameters below are all constant expressions but can be
+# MAGIC modified to widen the search space. For example, when training a decision tree classifier, to allow
+# MAGIC the maximum tree depth to be either 2 or 3, set the key of 'max_depth' to
+# MAGIC `hp.choice('max_depth', [2, 3])`. Be sure to also increase `max_evals` in the `fmin` call below.
+# MAGIC 
+# MAGIC See https://docs.databricks.com/applications/machine-learning/automl-hyperparam-tuning/index.html
+# MAGIC for more information on hyperparameter tuning as well as
+# MAGIC http://hyperopt.github.io/hyperopt/getting-started/search_spaces/ for documentation on supported
+# MAGIC search expressions.
+# MAGIC 
+# MAGIC For documentation on parameters used by the model in use, please see:
+# MAGIC https://lightgbm.readthedocs.io/en/stable/pythonapi/lightgbm.LGBMClassifier.html
+# MAGIC 
+# MAGIC NOTE: The above URL points to a stable version of the documentation corresponding to the last
+# MAGIC released version of the package. The documentation may differ slightly for the package version
+# MAGIC used by this notebook.
+
+# COMMAND ----------
+
+space = {
+  "colsample_bytree": 0.7992803427991522,
+  "lambda_l1": 2.5341265553434313,
+  "lambda_l2": 1.9323740993180571,
+  "learning_rate": 3.285339453574416,
+  "max_bin": 221,
+  "max_depth": 6,
+  "min_child_samples": 132,
+  "n_estimators": 2005,
+  "num_leaves": 452,
+  "path_smooth": 14.64649756390726,
+  "subsample": 0.681817839127397,
+  "random_state": 460409061,
+}
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Run trials
+# MAGIC When widening the search space and training multiple models, switch to `SparkTrials` to parallelize
+# MAGIC training on Spark:
+# MAGIC ```
+# MAGIC from hyperopt import SparkTrials
+# MAGIC trials = SparkTrials()
+# MAGIC ```
+# MAGIC 
+# MAGIC NOTE: While `Trials` starts an MLFlow run for each set of hyperparameters, `SparkTrials` only starts
+# MAGIC one top-level run; it will start a subrun for each set of hyperparameters.
+# MAGIC 
+# MAGIC See http://hyperopt.github.io/hyperopt/scaleout/spark/ for more info.
+
+# COMMAND ----------
+
+trials = Trials()
+fmin(objective,
+     space=space,
+     algo=tpe.suggest,
+     max_evals=1,  # Increase this when widening the hyperparameter search space.
+     trials=trials)
+
+best_result = trials.best_trial["result"]
+model = best_result["model"]
+mlflow_run = best_result["run"]
+
+display(
+  pd.DataFrame(
+    [best_result["val_metrics"], best_result["test_metrics"]],
+    index=["validation", "test"]))
+
+set_config(display="diagram")
+model
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Feature importance
+# MAGIC 
+# MAGIC SHAP is a game-theoretic approach to explain machine learning models, providing a summary plot
+# MAGIC of the relationship between features and model output. Features are ranked in descending order of
+# MAGIC importance, and impact/color describe the correlation between the feature and the target variable.
+# MAGIC - Generating SHAP feature importance is a very memory intensive operation, so to ensure that AutoML can run trials without
+# MAGIC   running out of memory, we disable SHAP by default.<br />
+# MAGIC   You can set the flag defined below to `shap_enabled = True` and re-run this notebook to see the SHAP plots.
+# MAGIC - To reduce the computational overhead of each trial, a single example is sampled from the validation set to explain.<br />
+# MAGIC   For more thorough results, increase the sample size of explanations, or provide your own examples to explain.
+# MAGIC - SHAP cannot explain models using data with nulls; if your dataset has any, both the background data and
+# MAGIC   examples to explain will be imputed using the mode (most frequent values). This affects the computed
+# MAGIC   SHAP values, as the imputed samples may not match the actual data distribution.
+# MAGIC 
+# MAGIC For more information on how to read Shapley values, see the [SHAP documentation](https://shap.readthedocs.io/en/latest/example_notebooks/overviews/An%20introduction%20to%20explainable%20AI%20with%20Shapley%20values.html).
+
+# COMMAND ----------
+
+# Set this flag to True and re-run the notebook to see the SHAP plots
+shap_enabled = False
+
+# COMMAND ----------
+
+if shap_enabled:
+    from shap import KernelExplainer, summary_plot
+    # SHAP cannot explain models using data with nulls.
+    # To enable SHAP to succeed, both the background data and examples to explain are imputed with the mode (most frequent values).
+    mode = X_train.mode().iloc[0]
+
+    # Sample background data for SHAP Explainer. Increase the sample size to reduce variance.
+    train_sample = X_train.sample(n=min(100, X_train.shape[0]), random_state=460409061).fillna(mode)
+
+    # Sample some rows from the validation set to explain. Increase the sample size for more thorough results.
+    example = X_val.sample(n=min(100, X_val.shape[0]), random_state=460409061).fillna(mode)
+
+    # Use Kernel SHAP to explain feature importance on the sampled rows from the validation set.
+    predict = lambda x: model.predict(pd.DataFrame(x, columns=X_train.columns))
+    explainer = KernelExplainer(predict, train_sample, link="identity")
+    shap_values = explainer.shap_values(example, l1_reg=False)
+    summary_plot(shap_values, example, class_names=model.classes_)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Inference
+# MAGIC [The MLflow Model Registry](https://docs.databricks.com/applications/mlflow/model-registry.html) is a collaborative hub where teams can share ML models, work together from experimentation to online testing and production, integrate with approval and governance workflows, and monitor ML deployments and their performance. The snippets below show how to add the model trained in this notebook to the model registry and to retrieve it later for inference.
+# MAGIC 
+# MAGIC > **NOTE:** The `model_uri` for the model already trained in this notebook can be found in the cell below
+# MAGIC 
+# MAGIC ### Register to Model Registry
+# MAGIC ```
+# MAGIC model_name = "Example"
+# MAGIC 
+# MAGIC model_uri = f"runs:/{ mlflow_run.info.run_id }/model"
+# MAGIC registered_model_version = mlflow.register_model(model_uri, model_name)
+# MAGIC ```
+# MAGIC 
+# MAGIC ### Load from Model Registry
+# MAGIC ```
+# MAGIC model_name = "Example"
+# MAGIC model_version = registered_model_version.version
+# MAGIC 
+# MAGIC model_uri=f"models:/{model_name}/{model_version}"
+# MAGIC model = mlflow.pyfunc.load_model(model_uri=model_uri)
+# MAGIC model.predict(input_X)
+# MAGIC ```
+# MAGIC 
+# MAGIC ### Load model without registering
+# MAGIC ```
+# MAGIC model_uri = f"runs:/{ mlflow_run.info.run_id }/model"
+# MAGIC 
+# MAGIC model = mlflow.pyfunc.load_model(model_uri=model_uri)
+# MAGIC model.predict(input_X)
+# MAGIC ```
+
+# COMMAND ----------
+
+# model_uri for the generated model
+print(f"runs:/{ mlflow_run.info.run_id }/model")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Confusion matrix, ROC and Precision-Recall curves for validation data
+# MAGIC 
+# MAGIC We show the confusion matrix, ROC and Precision-Recall curves of the model on the validation data.
+# MAGIC 
+# MAGIC For the plots evaluated on the training and the test data, check the artifacts on the MLflow run page.
+
+# COMMAND ----------
+
+# Paste the entire output (%md ...) to an empty cell, and click the link to see the MLflow run page
+print(f"%md [Link to model run page](#mlflow/experiments/1879520688768185/runs/{ mlflow_run.info.run_id }/artifactPath/model)")
+
+# COMMAND ----------
+
+import uuid
+from IPython.display import Image
+
+# Create temp directory to download MLflow model artifact
+eval_temp_dir = os.path.join(os.environ["SPARK_LOCAL_DIRS"], "tmp", str(uuid.uuid4())[:8])
+os.makedirs(eval_temp_dir, exist_ok=True)
+
+# Download the artifact
+eval_path = mlflow.artifacts.download_artifacts(run_id=mlflow_run.info.run_id, dst_path=eval_temp_dir)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Confusion matrix for validation dataset
+
+# COMMAND ----------
+
+eval_confusion_matrix_path = os.path.join(eval_path, "val_confusion_matrix.png")
+display(Image(filename=eval_confusion_matrix_path))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### ROC curve for validation dataset
+
+# COMMAND ----------
+
+eval_roc_curve_path = os.path.join(eval_path, "val_roc_curve.png")
+display(Image(filename=eval_roc_curve_path))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Precision-Recall curve for validation dataset
+
+# COMMAND ----------
+
+eval_pr_curve_path = os.path.join(eval_path, "val_precision_recall_curve.png")
+display(Image(filename=eval_pr_curve_path))
